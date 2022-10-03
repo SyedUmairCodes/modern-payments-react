ef6174611c27d89fee45343ddd256b101437261f
Syed Umair Ali <syedumairali.617@gmail.com>
2022-10-03 18:20:08 +0500

    Card deals section completed

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,24 @@
 import React from "react"
+import styles, {layout} from "../style.js";
+import Button from "./Button.jsx";
+import { card } from "../assets"
 
 const CardDeal = () => {
-    return <div>Billing</div>
+    return(
+        <section className={layout.section}>
+            <div className={layout.sectionInfo}>
+                <h2 className={styles.heading2}>Find a better card deal <br className="sm:block hidden"/> in a few easy steps.</h2>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>It is a long established fact that a reader will be
+                    distracted by the readable content of a page when looking at its layout.</p>
+                <Button styles="mt-10"/>
+            </div>
+
+        {/* Section image*/}
+            <div className={layout.sectionImg}>
+                <img src={card} alt="card" className="w-[100%] h-[100%]"/>
+            </div>
+        </section>
+    )
 }
 
 export default CardDeal
